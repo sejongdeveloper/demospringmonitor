5f3c4e6d13e1e20add7c1ce0cd2ccafea2b961f2
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-08 15:41:53 +0900

    스프링 부트 Actuator 3부: 스프링 부트 어드민

---
+++ src/test/java/me/whiteship/demospringmonitor/DemospringmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demospringmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
